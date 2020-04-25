faf6e03da7e54bf1116963652231c78672314a17
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-04-25 16:20:11 +0300

    color picker component changed

--- src/ui/ColorPicker.js
+++ src/ui/ColorPicker.js
@@ -3,11 +3,14 @@ import styles from '../styles/colorPicker.css';
 
 export default function ColorPicker(props) {
     return (
-        <ul className={styles.colorPicker}>{colorPicker}</ul>
+        <ul className={styles.colorPicker}>
+            {
+                colors.map((color, key) =>
+                    <li key={key} className={styles.color} style={{backgroundColor: color}}></li>
+                )
+            }
+        </ul>
     )
 }
 
 const colors = ['#F288AF', '#134BF2', '#F2A516', '#F2490C'];
-const colorPicker = colors.map((color, key) =>
-    <li key={key} className={styles.color} style={{backgroundColor: color}}></li>
-);
