857b0a898e99ecfbb23e5965141d40e2d0d5b0f6
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-04-27 17:54:25 +0300

    Title component added

---
+++ src/ui/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/title.css';
+
+export default function Title(props) {
+    return(
+        <input className={styles.title} maxLength="20">
+            
+        </input>
+    );
+}
