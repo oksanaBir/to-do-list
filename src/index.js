ff6f335f90bef3eca19982ad60ffb2c0851cc002
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-12 01:20:55 +0300

    index updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-ReactDOM.render(<div>Hey</div>, document.querySelector("#root"));
+import App from "./components/App";
+ReactDOM.render(<App />,  document.querySelector("#root"));
+
