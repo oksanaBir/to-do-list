96fc57df3e4215a2ee3bd8ff0718a49cd4755a7a
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-12 01:40:12 +0300

    color picker component changed

--- src/ui/ColorPicker.js
+++ src/ui/ColorPicker.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from '../styles/colorPicker.css';
 
-// const colors = ['#F0F0F0', '#F288AF', '#134BF2', '#F2A516', '#F2490C'];
+const colors = ['#F0F0F0', '#F288AF', '#134BF2', '#F2A516', '#F2490C'];
 
 export default function ColorPicker({changeColor, props}) {
     return (
         <ul className={styles.colorPicker}>
             {
                 colors.map((color, key) =>
-                    <li key={key} className={styles.color}  onClick={() => changeColor(color)}></li>
+                    <li key={key} className={styles.color}  onClick={() => changeColor(color)} style={{ backgroundColor: color }}></li>
                 )
             }
         </ul>
