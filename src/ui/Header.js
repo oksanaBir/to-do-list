63d2903baac5c64b6d8267980f453434bbfe5c5c
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-12 01:40:29 +0300

    header component changed

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from '../styles/header.css';
-import Sorting from './Sorting'
 
 export default function Header(props){
     return (
         <>
             <div className={styles.header}>{props.children}</div>
-            <Sorting />
         </>
     );
 }
