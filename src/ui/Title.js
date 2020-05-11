e730987f6408485f6873708e0b984abf8cc6b5fd
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-12 00:58:16 +0300

    title component changed

--- src/ui/Title.js
+++ src/ui/Title.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from '../styles/title.css';
 
-export default function Title(props) {
-    return(
-        <input className={styles.title} maxLength="20">
-            
-        </input>
-    );
+export default function Title({ children, contentEditable, handleChange }) {
+    if(contentEditable){
+        return (
+            <h2>{ children }</h2>
+        );
+    } else {
+        return(
+            <input className={styles.title} maxLength="20" onChange={handleChange} style={{ height: '22px' }}/>
+        );
+    }
 }
