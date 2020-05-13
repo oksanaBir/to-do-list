483004b82ec0901de330883b36b7f490fea96a4f
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-13 21:01:16 +0300

    ComponentsGallery changed

--- src/ui/ComponentsGallery.js
+++ src/ui/ComponentsGallery.js
@@ -2,27 +2,24 @@ import React from 'react';
 import Header from './Header';
 import NoteWrapper from './NoteWrapper';
 import Button from './Button';
-import { FlexBox, flexPositions} from './Flexbox';
+import { FlexBox, flexPositions, flexDirection} from './Flexbox';
 import Sorting from './Sorting';
 import ColorPicker from './ColorPicker';
 import Title from './Title';
+import NoteDescription from './NoteDescription';
 
 export default function ComponentsGallery() {
     return (
         <>
             <Header>Заметки</Header>
             <Sorting />
-            <FlexBox>
-                <FlexBox margin={20}>
-                    <Button>Добавить</Button>
-                </FlexBox>
-                <FlexBox position={flexPositions.end}>
-                    <NoteWrapper />
-                    <Title contentEditable={true}>Заголовок</Title>
-                    <ColorPicker />
-                    <Button>Изменить</Button>
-                    <Button>Удалить</Button>
-                </FlexBox>
+            <FlexBox direction={flexDirection.column} margin={10}>
+                <Button>Добавить</Button>
+                <NoteWrapper />
+                <NoteDescription handleClick={() => console.log('')}/>
+                <Title contentEditable={true}></Title>
+                <ColorPicker colors={['#F0F0F0', '#F288AF', '#134BF2', '#F2A516', '#F2490C']} changeColor={(key) => console.log(key)} />
+                <Button isDanger={true}>Удалить</Button>            
             </FlexBox>
         </>
     );
