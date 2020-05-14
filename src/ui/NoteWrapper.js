0c3e28d0d267f6d10c5fcb65bc511f91fe0c39ea
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-14 23:16:27 +0300

    NoteWrapper changed

--- src/ui/NoteWrapper.js
+++ src/ui/NoteWrapper.js
@@ -1,10 +1,10 @@
 import styles from '../styles/noteWrapper.css';
 import React from 'react';
 
-export default function NoteWrapper(props) {
+export default function NoteWrapper({children}) {
     return (
         <div className={styles.note}>
-            {props.children}
+            {children}
         </div>
     );
 }
