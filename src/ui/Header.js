518d8d3c3266f2408d4e8b4d3aa7209d41276ccb
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-14 17:03:03 +0300

    Header changed

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -3,8 +3,6 @@ import styles from '../styles/header.css';
 
 export default function Header(props){
     return (
-        <>
-            <div className={styles.header}>{props.children}</div>
-        </>
+        <div className={styles.header}>{props.children}</div>
     );
 }
