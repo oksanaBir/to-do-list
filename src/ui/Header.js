d0f45a0779841fdb19e9fdc03847521fe39460ef
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-14 23:13:17 +0300

    Header changed

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from '../styles/header.css';
 
-export default function Header(props){
+export default function Header({children}){
     return (
-        <div className={styles.header}>{props.children}</div>
+        <div className={styles.header}>{children}</div>
     );
 }
