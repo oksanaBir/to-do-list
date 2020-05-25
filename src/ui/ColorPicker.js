0cc00969ae7f28250c5775dc9b35f1bee20e0776
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-25 17:26:55 +0300

    selected color added

--- src/ui/ColorPicker.js
+++ src/ui/ColorPicker.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from '../styles/colorPicker.css';
+import classNames from 'classnames/bind';
 
-export default function ColorPicker({changeColor, colors}) {
+let cx = classNames.bind(styles);
+
+export default function ColorPicker({changeColor, colors, selectedColor}) {
     return (
         <ul className={styles.colorPicker}>
             {
                 colors.map((color, key) =>
                     <li 
                         key={key}
-                        className={styles.color}
-                        onClick={() => changeColor(key)}
+                        className={cx({
+                            color: true,
+                            selectedColor: key === selectedColor,
+                        })}
                         style={{ backgroundColor: color }}
+                        onClick={() => changeColor(key)}
                     />
                 )
             }
