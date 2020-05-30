417a6ca301f721ade3a4ba40dda651cd6639236a
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-30 20:45:31 +0300

    added scrollbar

--- src/ui/NoteDescription.js
+++ src/ui/NoteDescription.js
@@ -1,10 +1,31 @@
 import styles from '../styles/noteDescription.css';
 import React from 'react';
 
-export default function NoteDescription({children, handleChange}){
-    return(
-        <textarea className={styles.noteDescription} onChange={handleChange}>
-            {children}
-        </textarea>
-    )
+export default function NoteDescription({children, handleChange, backgroundColor, contentEditable}){
+    if(contentEditable){
+        return(
+            <textarea
+                className={styles.noteDescription}
+                onChange={handleChange}
+                style={{ 
+                    backgroundColor,
+                    ScrollbarBaseColor: backgroundColor,
+                    ScrollbarBaseColor: 'red'
+                }}
+                placeholder="Текст заметки"
+            >
+                {children}
+            </textarea>            
+        )
+    } else {
+        return (
+            <span
+                disabled="disabled"
+                className={styles.noteDescription}
+                style={{ backgroundColor, paddingTop: '19px', height: '149px' }}
+            >
+                {children}
+            </span>
+        );
+    }
 }
