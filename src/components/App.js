f242c89e1144e8df53a20ca3ed59ebd1da741546
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-30 20:49:50 +0300

    state and methods for notes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,93 @@
 import React from 'react';
-import ComponentsGallery from '../ui/ComponentsGallery';
+import Note from './Note';
+import Button from '../ui/Button';
+import Header from '../ui/Header';
+import {FlexBox, flexPositions, flexDirection} from '../ui/Flexbox';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            sortingParams: [
+                { label: 'по цвету', value: 'color', },
+                { label: 'по дате создания', value: 'date', },
+                { label: 'по названию', value: 'name' },
+            ],
+            colors: ['#F0F0F0', '#F0D1E2', '#D0D8F5', '#F5E6BA', '#F5C0BA'],
+            notes: {},
+        }
+
+        this.deleteNote = this.deleteNote.bind(this);
+        this.createNote = this.createNote.bind(this);
+        this.editNote = this.editNote.bind(this);
+    }
+
+    componentDidMount() {
+        this.createNote();
+    }
+
+    createNote() {
+        this.setState({
+            notes: {
+                ...this.state.notes,
+                [Date.now().toString() + Math.random().toString()]: {
+                    color: 0,
+                    title: ' ',
+                    isEditable: true,
+                    value: ' ',
+                    description: '',
+                }
+            }
+        });
+    }
+
+    deleteNote(noteId) {
+        delete this.state.notes[noteId]
+        this.setState({
+            notes: {
+                ...this.state.notes,
+            }
+        });
+    }
+
+    editNote(noteId, field, value) {
+        this.setState({
+            notes: {
+                ...this.state.notes,
+                [noteId]: {
+                    ...this.state.notes[noteId],
+                    [field]: value,
+                },
+            },
+        });
     }
 
     render() {
+        const notesIds = Object.keys(this.state.notes);
+
         return (
-            <ComponentsGallery />
+            <>
+                <Header>Заметки</Header>
+                <FlexBox direction={flexDirection.row}>
+                    <Button handleClick={this.createNote}>Добавить</Button>
+                    {
+                        notesIds.map(id => {
+                            return (
+                                <Note
+                                    key={id}
+                                    noteId={id}
+                                    note={this.state.notes[id]}
+                                    changeColor={this.changeColor}
+                                    colors={this.state.colors}
+                                    deleteNote={this.deleteNote}
+                                    editNote={this.editNote}
+                                />
+                            )
+                        })
+                    }
+                    {/* <Sorting sortingParams={this.state.sortingParams} /> */}
+                </FlexBox>
+            </>
         );
     }
 }
