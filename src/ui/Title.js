e824e951140767ed286df809f777b8c0d7ab347e
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-05-30 20:44:06 +0300

    Title background changed

--- src/ui/Title.js
+++ src/ui/Title.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from '../styles/title.css';
 
-export default function Title({value, contentEditable, handleChange}) {
+export default function Title({value, contentEditable, handleChange, backgroundColor}) {
     if(contentEditable){
         return (
             <input
                 className={styles.title}
+                placeholder="Заголовок"
                 maxLength="20"
                 value={value}
-                style={{ height: '22px' }}
+                style={{ 
+                    height: '22px',
+                    backgroundColor
+                }}
                 onChange={handleChange}
-            />      
+            />
         );
     } else {
         return(
