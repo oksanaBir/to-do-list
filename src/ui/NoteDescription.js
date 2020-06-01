c8e49f56e55ac1cf0e58907ab9a1aa128aebdd83
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-06-01 13:26:43 +0300

    note description added to component

--- src/ui/NoteDescription.js
+++ src/ui/NoteDescription.js
@@ -1,21 +1,20 @@
 import styles from '../styles/noteDescription.css';
 import React from 'react';
 
-export default function NoteDescription({children, handleChange, backgroundColor, contentEditable}){
+export default function NoteDescription({value, handleChange, backgroundColor, contentEditable}){
     if(contentEditable){
         return(
             <textarea
                 className={styles.noteDescription}
-                onChange={handleChange}
+                onChange={() => handleChange(event)}
                 style={{ 
                     backgroundColor,
                     ScrollbarBaseColor: backgroundColor,
                     ScrollbarBaseColor: 'red'
                 }}
                 placeholder="Текст заметки"
-            >
-                {children}
-            </textarea>            
+                value={value}
+            />
         )
     } else {
         return (
@@ -23,8 +22,8 @@ export default function NoteDescription({children, handleChange, backgroundColor
                 disabled="disabled"
                 className={styles.noteDescription}
                 style={{ backgroundColor, paddingTop: '19px', height: '149px' }}
-            >
-                {children}
+            > {value.replace(/\n/g, '</ br>')}
+        
             </span>
         );
     }
