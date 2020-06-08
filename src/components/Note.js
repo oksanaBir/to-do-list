1c974568335f6f6a832591ea433d0d326e139fd6
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-06-08 14:20:35 +0300

    validation and Date component added

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,6 +4,7 @@ import ColorPicker from '../ui/ColorPicker';
 import Title from '../ui/Title';
 import NoteDescription from '../ui/NoteDescription';
 import Button from '../ui/Button';
+import Date from '../ui/Date';
 
 export default class Note extends React.Component {
     constructor(props){
@@ -11,24 +12,56 @@ export default class Note extends React.Component {
     }
 
     onColorChange(noteId, newColor) {
-        this.props.editNote(noteId, 'color', newColor);
+        this.props.editNote(
+            noteId,
+            {
+                color: newColor,
+            }
+        );
     }
 
-    onEditableChange(noteId, isEditable) {
-        this.props.editNote(noteId, 'isEditable', !isEditable)
-    }
+    onEditableChange(noteId, isEditable, titleValidation, dateValidation) {
+        titleValidation & dateValidation === true &&
+            this.props.editNote(
+                noteId,
+                {
+                    isEditable: !isEditable
+                }
+            );
+        }
 
     onTitleChange(event, noteId) {
-        this.props.editNote(noteId, 'title', event.target.value);
+        this.props.editNote(
+            noteId,
+            {
+                title: event.target.value,
+                titleValidation: event.target.value !== undefined,
+            }
+        );
     }
 
     onDescriptionChange(event, noteId) {
-        this.props.editNote(noteId, 'description', event.target.value)
+        this.props.editNote(
+            noteId,
+            {
+                description: event.target.value,
+            }
+        );
+    }
+
+    onDateChange(event, noteId) {
+        this.props.editNote(
+            noteId,
+            {
+                date: event.target.value,
+                dateValidation: event.target.value !== undefined,
+            }
+        );
     }
 
     render() {
         const { noteId, colors, note, deleteNote } = this.props;
-        const { isEditable, title, description } = note;
+        const { isEditable, title, description, date, titleValidation, dateValidation } = note;
         const activeColor = colors[note.color];
 
         return(
@@ -37,13 +70,21 @@ export default class Note extends React.Component {
                     backgroundColor={activeColor}
                     contentEditable={isEditable}
                     value={title}
-                    handleChange={(event) => this.onTitleChange(event, noteId)}             
+                    validation={titleValidation}
+                    handleChange={(event) => this.onTitleChange(event, noteId)}
+                    
                 ></Title>
+                <Date
+                    contentEditable={isEditable}
+                    value={date}
+                    handleChange={(event) => this.onDateChange(event, noteId)}
+                    validation={dateValidation}
+                />
                 <NoteDescription
                     contentEditable={isEditable}
                     backgroundColor={activeColor}
                     value={description}
-                    handleChange={(event) => this.onDescriptionChange(event, noteId)}
+                    handleChange={(event) => this.onDescriptionChange(event, noteId, validation)}
                 />
                 {
                     isEditable &&
@@ -53,7 +94,7 @@ export default class Note extends React.Component {
                             selectedColor={note.color}
                         />
                 }
-                <Button handleClick={() => this.onEditableChange(noteId, isEditable)}>
+                <Button handleClick={() => this.onEditableChange(noteId, isEditable, titleValidation, dateValidation)}>
                     { isEditable ? 'Сохранить' : 'Изменить' }
                 </Button>
                 <Button handleClick={() => deleteNote(noteId)} isDanger={true}>Удалить</Button>
