988e89fdec22de52bf2c7784adbcee5637d4fd7e
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-06-13 01:48:42 +0300

    Sorting added

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import Select from '../ui/Select';
 
+export const sortParams = [
+    { label: 'по цвету', value: 'color', },
+    { label: 'по дате выполнения', value: 'date', },
+    { label: 'по названию', value: 'name', },
+];
+
+export const addSortParams = [
+    { label: 'с начала', value: 'start', },
+    { label: 'с конца', value: 'end', },
+];
+
+export const dateSortParams = [
+    { label: 'сначала новые', value: 'start', },
+    { label: 'сначала старые', value: 'end', },
+];
+
+
 export default class Sorting extends React.Component {
     constructor(props){
         super(props)
-        
     }
+
     render(){
         return(
-            <Select />
+            <>
+                <Select
+                    sortParams={sortParams}
+                    value={this.props.value}
+                    onChange={(event) => this.props.onSortChange(event)}
+                    label={this.props.label}
+                />
+                <Select
+                    sortParams={
+                        this.props.value == 'date' ? dateSortParams : addSortParams
+                    }
+                    value={this.props.additionalValue}
+                    onChange={(event) => this.props.onSortDirectionChange(event)}
+                />
+            </>
         );
     }
 }
--- src/ui/Select.js
+++ src/ui/Select.js
@@ -1,17 +1,16 @@
 import styles from '../styles/select.css';
 import React from 'react';
 
-export default function Select({handleChange, sortingParams}) {
+export default function Select({onChange, sortParams, value}) {
     return (
-        <select className={styles.sorting}>
+        <select className={styles.sorting} onChange={() => onChange(event)} value={value}>
             { 
-                sortingParams.map((sortingParams, key) => 
+                sortParams.map((sortParam, key) => 
                     <option
                         key={key}
                         className={styles.sortingParams}
-                        value={sortingParams.value}
-                        onChange={() =>handleChange(key)}    
-                    >{sortingParams.label}</option>
+                        value={sortParam.value}
+                    >{sortParam.label}</option>
                 )
             }
         </select>
