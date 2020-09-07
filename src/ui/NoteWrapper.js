bdb527076081560dd5130aa29f4eec072642bde0
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-07 14:12:43 +0300

    css changed to less

--- src/ui/NoteWrapper.js
+++ src/ui/NoteWrapper.js
@@ -1,14 +1,14 @@
-import styles from '../styles/noteWrapper.css';
+import styles from '../styles/noteWrapper.less';
 import React from 'react';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
 export default function NoteWrapper({children, backgroundColor, contentEditable}) {
-    let className = cx({
-        note: true,
-        editableNote: contentEditable
-    });
+    let className = cx(
+        'note',
+        { editableNote: contentEditable },
+    );
 
     return (
         <div
