8fb699fabede57de96f651576d3d17a69ba79abd
Oksana Biryukova <biryukova.oksana49@gmail.com>
2020-09-09 14:14:52 +0300

    mainParam deleted

--- src/ui/Select.js
+++ src/ui/Select.js
@@ -1,14 +1,14 @@
-import styles from '../styles/select.less';
 import React from 'react';
+import classNames from 'classnames';
 
 export default function Select({onChange, sortParams, value}) {
     return (
-        <select className={styles.sort} onChange={(event) => onChange(event.target.value)} value={value}>
+        <select className={classNames('sort')} onChange={(event) => onChange(event.target.value)} value={value}>
             { 
                 sortParams.map((sortParam, key) => 
                     <option
                         key={key}
-                        className={styles.sortParams}
+                        className={classNames('sortParams')}
                         value={sortParam.value}
                     >{sortParam.label}</option>
                 )
